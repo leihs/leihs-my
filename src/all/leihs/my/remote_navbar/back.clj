6d629379c78e846aa4768c47494c66a28db4b6b5
Matus Kmit <matus.kmit@zhdk.ch>
2018-11-02 17:00:54 +0100

    sub apps

--- src/all/leihs/my/remote_navbar/back.clj
+++ src/all/leihs/my/remote_navbar/back.clj
@@ -9,35 +9,46 @@
              [borrow-access? managed-inventory-pools]]
             [leihs.core.user.permissions.procure :as procure]))
 
-(defn- navbar-main-sections-html
-  [tx auth-entity]
-  (html
-    [:h3 {:style "color: red"} "Remote Navbar"]
-    [:ul
-     (if (borrow-access? tx auth-entity)
-       [:li [:a {:href (path :borrow)} "Borrow"]])
-     (if (:is_admin auth-entity) [:li [:a {:href (path :admin)} "Admin"]])
-     (if (procure/any-access? tx auth-entity)
-       [:li [:a {:href (path :procurement)} "Procurement"]])
-     (let [pools (managed-inventory-pools tx auth-entity)]
-       (if-not (empty? pools)
-         [:div "Pools:"
-          [:ul
-           (map #(vector :li
-                         [:a {:href (path :daily {:inventory_pool_id (:id %)})}
-                          (:name %)])
-             pools)]]))]))
+; (defn- navbar-main-sections-html
+;   [tx auth-entity]
+;   (html
+;     [:h3 {:style "color: red"} "Remote Navbar"]
+;     [:ul
+;      (if (borrow-access? tx auth-entity)
+;        [:li [:a {:href (path :borrow)} "Borrow"]])
+;      (if (:is_admin auth-entity) [:li [:a {:href (path :admin)} "Admin"]])
+;      (if (procure/any-access? tx auth-entity)
+;        [:li [:a {:href (path :procurement)} "Procurement"]])
+;      (let [pools (managed-inventory-pools tx auth-entity)]
+;        (if-not (empty? pools)
+;          [:div "Pools:"
+;           [:ul
+;            (map #(vector :li
+;                          [:a {:href (path :daily {:inventory_pool_id (:id %)})}
+;                           (:name %)])
+;              pools)]]))]))
 
-(defn- navbar-main-sections
+(defn- sub-apps
   [tx auth-entity]
-  [(if (borrow-access? tx auth-entity) {:title "Borrow", :href "/borrow"})])
+  [{:borrow (borrow-access? tx auth-entity)}
+   {:admin (:is_admin auth-entity)}
+   {:procure (procure/any-access? tx auth-entity)}
+   {:manage (map #(hash-map :name (:name %)
+                            :href (path :daily {:inventory_pool_id (:id %)}))
+                 (managed-inventory-pools tx auth-entity))}])
+
+; (defn- navbar-main-sections
+;   [tx auth-entity]
+;   [(if (borrow-access? tx auth-entity) {:title "Borrow", :href "/borrow"})])
 
 (defn handler
   [request]
   (let [tx (:tx request)
         auth-entity (:authenticated-entity request)
-        html-arg (navbar-main-sections tx auth-entity)]
+        ; html-arg (navbar-main-sections tx auth-entity)
+        ]
     {:headers {"Content-Type" "text/plain"},
      :body (js-engine/render-react "Navbar"
                                    {:config {:appTitle "Matus",
-                                             :appColor "orange"}})}))
+                                             :appColor "orange"
+                                             :subApps (sub-apps tx auth-entity)}})}))
