5b5e53af3b52bccb3826bb1b70dc44dbfb6dfd0b
Matus Kmit <matus.kmit@zhdk.ch>
2018-11-05 14:23:34 +0100

    remote navbar

--- src/all/leihs/my/remote_navbar/back.clj
+++ src/all/leihs/my/remote_navbar/back.clj
@@ -1,15 +1,5 @@
 (ns leihs.my.remote-navbar.back
-  (:require [clojure.java.jdbc :as jdbc]
-            [clojure.tools.logging :as log]
-            [hiccup.core :refer [html]]
-            [leihs.my.server-side-js.engine :as js-engine]
-            [leihs.core.sql :as sql]))
+  (:require [leihs.my.back.ssr :refer [render-navbar]]))
 
-
-(defn handler
-  [request]
-    {:headers {"Content-Type" "text/plain"},
-     :body (js-engine/render-react
-             "Navbar"
-             {:config {:appTitle "TODO",
-                       :appColor "red"}})})
+(defn handler [request]
+  (render-navbar request))
