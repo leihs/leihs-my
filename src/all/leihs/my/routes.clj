1e43d8f191f238648f7be073bc26ee9b1ff8962a
Matus Kmit <matus.kmit@zhdk.ch>
2018-11-06 17:01:42 +0100

    debug routes

--- src/all/leihs/my/routes.clj
+++ src/all/leihs/my/routes.clj
@@ -135,6 +135,6 @@
       ring-exception/wrap))
 
 ;#### debug ###################################################################
-;(logging-config/set-logger! :level :debug)
+(logging-config/set-logger! :level :debug)
 ;(logging-config/set-logger! :level :info)
-;(debug/debug-ns *ns*)
+(debug/debug-ns *ns*)
