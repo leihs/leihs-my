87f47cfe8e20d61b6f0ebe8aeb7ebf81f87ea699
Thomas Schank <DrTom@schank.ch>
2019-02-25 16:39:43 +0100

    Implement screenshots on spec fail and CI attachment thereof

--- spec/config/web.rb
+++ spec/config/web.rb
@@ -1,8 +1,10 @@
-require 'pry'
 require 'capybara/rspec'
-require 'selenium-webdriver'
 require 'faraday'
 require 'faraday_middleware'
+require 'pathname'
+require 'pry'
+require 'selenium-webdriver'
+require 'fileutils'
 
 def base_url
   @base_url ||= ENV['LEIHS_MY_HTTP_BASE_URL'].presence || 'http://localhost:3240'
@@ -53,6 +55,42 @@ RSpec.configure do |config|
     set_capybara_values
     set_browser example
   end
+
+
+  config.after(:each) do |example|
+    unless example.exception.nil?
+      take_screenshot screenshot_dir
+    end
+  end
+
+  config.before :all do 
+    FileUtils.remove_dir(screenshot_dir, force: true)
+    FileUtils.mkdir_p(screenshot_dir)
+  end
+
+  def tmp_dir
+    Pathname File.absolute_path(Pathname(__FILE__).join("..","..","..","tmp"))
+  end
+
+  def screenshot_dir
+    tmp_dir.join('screenshots')
+  end
+
+  def take_screenshot(screenshot_dir = nil, name = nil)
+    name ||= "#{Time.now.iso8601.tr(':', '-')}.png"
+    path = screenshot_dir.join(name)
+    case Capybara.current_driver
+    when :selenium
+      page.driver.browser.save_screenshot(path) rescue nil
+    when :poltergeist
+      page.driver.render(path, full: true) rescue nil
+    else
+      Logger.warn "Taking screenshots is not implemented for \
+              #{Capybara.current_driver}."
+    end
+  end
+
+
 end
 
 
