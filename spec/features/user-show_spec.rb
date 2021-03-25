40736069aeb58a8a0fe94124a8fb8245b607206c
Max Albrecht <max.albrecht@zhdk.ch>
2021-03-25 11:01:56 +0100

    show message on "me" page if no access to any pools

--- spec/features/user-show_spec.rb
+++ spec/features/user-show_spec.rb
@@ -15,6 +15,21 @@ feature 'User show', type: :feature do
     expect(page).to have_content "User #{@user.firstname} #{@user.lastname}"
   end
 
+  scenario 'Show message when no acess to any pool' do
+    err_msg = "You can not use the borrow section because you dont have access rights to any inventory pool!\nPlease contact your support or lending desk."
+    # ensure there are no access rights:
+    expect(AccessRight.where(user: @user).count).to be 0
+    sign_in_as @user
+    visit '/my/user/me'
+    expect(page).to have_selector('.ui-no-access-rights-warning', text: err_msg)
+
+    # create an access right and ensure the message is not show anymore:
+    FactoryBot.create :access_right, user: @user
+    expect(AccessRight.where(user: @user).count).to be 1
+    visit '/my/user/me'
+    expect(page).to have_no_selector('.ui-no-access-rights-warning', text: err_msg)
+  end
+
   scenario 'Details of another user' do
     sign_in_as @admin
     visit "/my/user/#{@user.id}"
