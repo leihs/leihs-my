cc8c8c2936f89f3b0203312026040bd86810e4c4
Matus Kmit <matuskmit1@gmail.com>
2021-08-27 13:02:16 +0200

    modify delegation factory

--- spec/factories/delegations.rb
+++ spec/factories/delegations.rb
@@ -1,7 +1,7 @@
 class Delegation < Sequel::Model(:users)
 end
 
-FactoryBot.define do
+FactoryBot.modify do
   factory :delegation do
     firstname { Faker::Name.last_name }
     delegator_user_id { User.all.sample.id }
