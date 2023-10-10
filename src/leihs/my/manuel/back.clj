a08e18695ade46fa483727fd928066a97717ae21
mradl <manuel.radl@zhdk.ch>
2023-10-10 14:43:31 +0200

    test-endpoint: http://localhost:3240/my/manuel?mode=<json|csv|stream>  default: text

--- src/leihs/my/manuel/back.clj
+++ src/leihs/my/manuel/back.clj
@@ -1,35 +1,79 @@
 (ns leihs.my.manuel.back
-  (:refer-clojure :exclude [str keyword])
-  (:require
-    [leihs.core.core :refer [keyword str presence]]
-    [leihs.core.sql :as sql]
+    (:refer-clojure :exclude [keyword str])
+    (:require
+      [clojure.java.jdbc :as jdbc]
+      [clojure.tools.logging :as logging]
 
-    [leihs.my.paths :refer [path]]
-    [leihs.my.user.shared :refer [set-password]]
+      [compojure.core :as cpj]
+      [leihs.core.core :refer [keyword presence str]]
 
-    [clojure.java.jdbc :as jdbc]
-    [compojure.core :as cpj]
-    [ring.util.response :refer [redirect]]
+      [leihs.core.sql :as sql]
+      [leihs.my.paths :refer [path]]
 
-    [clojure.tools.logging :as logging]
-    [logbug.debug :as debug]))
+      ;TODO: askMatus
+      ;[logbug.debug :as debug]
+      [taoensso.timbre :refer [debug info warn error spy]]
 
-(defn fetch-first-user [tx]
-  (-> (sql/select :*)
-      (sql/from :users)
-      (sql/limit 1)
-      sql/format
-      (->> (jdbc/query tx))
-      first))
+      [ring.util.response :refer [redirect]]))
+
+(defn extract-as-csv "extract a couple of attr to csv-format" [e]
+      (let [
+            firstname (:firstname e)
+            lastname (:lastname e)
+            city (:city e)
+            zip (:zip e)
+            phone (:phone e)
+            title "firstname;lastname;city;zip;phone\n"
+            joined (clojure.string/join ";" [firstname lastname city zip phone])
+            result (str title joined)
+            ]
+
+           (debug  (str ">> myDebug 01: " result))
+           (println  (str ">> myDebug 02: " result))
+
+           result))
+
+
+
+(defn fetch-first-user-by-mode "extract first user-entry"
+
+      ([tx mode] (let [entry (fetch-first-user-by-mode tx)]
+                      (println "fetch-first-user::mode" mode)
+                      (cond (= mode "csv") (extract-as-csv entry)
+                            :else entry
+                            )))
+
+      ([tx]
+       (-> (sql/select :*)
+           (sql/from :users)
+           (sql/limit 1)
+           sql/format
+           (->> (jdbc/query tx))
+           first)))
 
 (defn fetch-first-user-next [tx-next]
-  {:user "user"})
+      {:user "user"})
 
-(defn manuel-get [request]
-  (let [tx (:tx request)]
-    {:status 200
-     :headers {"Content-Type" "text/html"},
-     :body (fetch-first-user tx)}))
+(defn manuel-get "extract 'mode=<json|csv|stream>'  default: text" [request]
+      (println request)
+      (println "query-params: " (:query-params request))
+      (println "mode:" (:mode (:query-params request)))
+
+
+      (let [tx (:tx request)
+            query-params (:query-params request)
+            mode-raw (:mode query-params)
+            mode (cond (= mode-raw nil) "text/html"
+                       (= mode-raw "json") "application/json"
+                       (= mode-raw "csv") "text/csv"
+                       (= mode-raw "stream") "application/octet-stream"
+                       :else "text/html"
+                       )]
+
+           {:status  200
+            :headers {"Content-Type" mode},
+            :body    (fetch-first-user-by-mode tx mode-raw)}
+           ))
 
 (def routes
   (cpj/routes
