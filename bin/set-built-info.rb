c4edb0fb31a733a2f003e534ae263116cce0f6c6
Matus Kmit <matuskmit1@gmail.com>
2026-05-06 14:28:38 +0200

    chore: commit sha version link logic

--- bin/set-built-info.rb
+++ bin/set-built-info.rb
@@ -3,6 +3,7 @@
 # NOTE: if git info already given via env vars, dont use git at all!
 commit_id = ENV['BUILD_COMMIT_ID']
 tree_id = ENV['BUILD_TREE_ID']
+super_commit_id = ENV['BUILD_SUPER_COMMIT_ID']
 
 require 'date'
 require 'pathname'
@@ -27,9 +28,20 @@ unless present?(commit_id) && present?(tree_id)
   tree_id = head.gtree.objectish
 end
 
+unless present?(super_commit_id)
+  require 'git'
+  super_commit_id = begin
+    super_project = Git.open(PROJECT_DIR.join('..'))
+    super_project.log[0].sha
+  rescue
+    nil
+  end
+end
+
 IO.write(PROJECT_DIR.join("resources").join("built-info.yml"),
          { 'commit_id' => commit_id,
            'hostname' => Socket.gethostname,
            'os' => Gem::Platform.local.to_s,
+           'super_commit_id' => super_commit_id,
            'timestamp' => DateTime.now.iso8601,
            'tree_id' => tree_id }.to_yaml)
